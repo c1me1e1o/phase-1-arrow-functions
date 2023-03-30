60db55834d711b326cd8e582a7ce9f669bbfa810
Carlmelo <car1melo@hotmail.com>
2023-03-29 22:32:03 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide(){
+let a = 2000;
+let b = 100;
+return a / b;
+}
+
+const square = (para1) => {
+    return para1 * para1;
+}
+
+const add = (para1, para2) => {
+    return para1 + para2;
+}
+
